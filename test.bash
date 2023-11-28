6ca8d00e8758dd227bbbccbaad244fba4c745b83
yamakouAO <yamaguchiao1031@gmail.com>
2023-11-29 02:24:27 +0900

    yamada.bash見て書き換えてみた

--- test.bash
+++ test.bash
@@ -1,8 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ko yamaguchi
+# SPDX_License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	ret=1
+}
+
+ret=0
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng "$LINENO"
+[ "${out}" = 14 ] || ng "$LINENO"
+
+exit $ret
 
 
 
