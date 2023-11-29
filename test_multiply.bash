552b05a05a7fe5d0656c43864453b540ea51d727
yamakouAO <yamaguchiao1031@gmail.com>
2023-11-30 03:46:33 +0900

    修正

--- test_multiply.bash
+++ test_multiply.bash
@@ -3,15 +3,26 @@
 # SPDX-License-Identifier: BSD-3-Cluause
 
 ng () {
-	echo ${1}が違います
+	echo ${1}
 	rat=1
 }
 
+
 rat=0
 
-out=$(seq 5 |./multiply)
+out=$(seq 5 | ./multiply)
 
-[ "${out}" = 120 ] || ng "${seq5}"
+[ "${out}" = 120 ] || ng "seq 5 が違います"
+
+out=$(echo あ | ./multiply )
+
+[ "$?" = 1 ] || ng "1"
+[ "${out}" = "" ] || ng "2"
+
+out=$(echo | ./multiply )
+
+[ "$?" = 1 ] || ng "3"
+[ "${out}" = "" ] || ng "4"
 
 [ "$rat" = 0 ] && echo OK
 exit $rat
