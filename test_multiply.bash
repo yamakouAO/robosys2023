fa4b376b903a39d42ee49cd83a8e537e68ee44f8
yamakouAO <yamaguchiao1031@gmail.com>
2023-11-30 02:08:39 +0900

    修正

--- test_multiply.bash
+++ test_multiply.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ko Yamaguchi
+# SPDX-License-Identifier: BSD-3-Cluause
+
+ng () {
+	echo ${1}が違います
+	rat=1
+}
+
+rat=0
 
 out=$(seq 5 |./multiply)
+
+[ "${out}" = 120 ] || ng "${seq5}"
+
+[ "$rat" = 0 ] && echo OK
+exit $rat
 
